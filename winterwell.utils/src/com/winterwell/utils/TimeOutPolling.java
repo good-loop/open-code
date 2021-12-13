1454b5faade618883f5e2f716aaccef5ec340fa7
Daniel Winterstein <daniel@good-loop.com>
2021-12-13 19:57:18 +0000

    snc

---
+++ winterwell.utils/src/com/winterwell/utils/TimeOutPolling.java
@@ -0,0 +1,14 @@
+package com.winterwell.utils;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.winterwell.utils.threads.SlowActor;
+
+/**
+ * Scalable time out system which only creates one extra thread
+ * @author daniel
+ *
+ */
+public class TimeOutPolling extends SlowActor<Thread> {
+	
+}
