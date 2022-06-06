8f9ab0552d3ab1cc4d69bc515fabbb4086919432
Daniel Winterstein <daniel@good-loop.com>
2022-06-06 22:25:34 +0100

    a bit quieter

--- youagain-java-client/src/com/winterwell/youagain/client/ShareClient.java
+++ youagain-java-client/src/com/winterwell/youagain/client/ShareClient.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 import org.eclipse.jetty.util.ajax.JSON;
 
 import com.winterwell.utils.Utils;
+import com.winterwell.utils.Warning;
 import com.winterwell.utils.containers.ArrayMap;
 import com.winterwell.utils.containers.Containers;
 import com.winterwell.utils.log.Log;
@@ -60,7 +61,7 @@ public final class ShareClient {
 				return Arrays.stream((Object[]) shares).map(share -> (String) SimpleJson.get(share, "item")).collect(Collectors.toList());
 			}
 		} catch (WebEx.E401 e401) {
-			Log.d("ShareClient.getSharedWith", e401);
+			Log.i("ShareClient.getSharedWith", new Warning(e401.toString()));
 			return Collections.emptyList();	
 		}
 		return Collections.emptyList();
