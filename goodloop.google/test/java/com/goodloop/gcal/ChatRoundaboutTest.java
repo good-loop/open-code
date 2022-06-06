da2eaf48c85bd29689d6f2d2632ea22fbb4af0fa
Wing Wong <wing@good-loop.com>
2022-06-06 17:31:27 +0100

    all staff list imported

--- goodloop.google/test/java/com/goodloop/gcal/ChatRoundaboutTest.java
+++ goodloop.google/test/java/com/goodloop/gcal/ChatRoundaboutTest.java
@@ -11,6 +11,7 @@ import javax.mail.internet.InternetAddress;
 
 import org.junit.Test;
 
+import com.goodloop.gcal.chatroundabout.ChatRoundabout;
 import com.goodloop.gcal.chatroundabout.ChatRoundaboutConfig;
 import com.winterwell.utils.Dep;
 import com.winterwell.utils.io.FileUtils;
@@ -24,45 +25,51 @@ import com.winterwell.web.email.SimpleMessage;
 public class ChatRoundaboutTest extends AMain<ChatRoundaboutConfig> {
 	
 	@Test
-	public void testEmailer() {
-		LocalDate _nextFriday = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.FRIDAY));
-		Time nextFriday = new Time(_nextFriday.toString());
-		
-		String logString = "Testing Emailer";
-		
-		// Send email
-		if (initFlag) return;
-		initFlag = true;
-		File propsFile = new File(FileUtils.getWinterwellDir(), "open-code/goodloop.google/config/email.properties");
-		if ( ! propsFile.exists()) {
-			propsFile = new File(FileUtils.getWinterwellDir(), "logins/local.properties");
-			if ( ! propsFile.exists()) {
-				System.out.println("Please make a file with email login details here: "+propsFile);
-				throw new ConfigException("Please symlink the logins/local.properties file or make a file with email login details here: "+propsFile+".");
-			}
-		}
-		Properties props = FileUtils.loadProperties(propsFile);				
-		EmailConfig ec = new EmailConfig();
-		ec.emailServer = props.getProperty("emailServer").trim();
-		ec.emailFrom = props.getProperty("emailFrom").trim();
-		ec.emailPassword = props.getProperty("emailPassword").trim();
-		ec.emailPort = 465;
-		ec.emailSSL = true;		
-		
-		Emailer emailer = new Emailer(ec);
-		String appName = "ChatRoundabout";
-		String subject = appName+": Weekly Report "+nextFriday.toISOStringDateOnly();
-		StringBuilder body = new StringBuilder();
-		body.append("\r\nChatRoundabout ran on :"+nextFriday.toISOStringDateOnly());
-		body.append("\r\n\r\n"+logString+"\r\n");
-		
-		InternetAddress from = emailer.getBotEmail();
-		from.setAddress("wing@good-loop.com");
-		InternetAddress email = emailer.getBotEmail();
-		email.setAddress("wing@good-loop.com");
-		SimpleMessage msg = new SimpleMessage(from, email, subject, body.toString());
-		emailer.send(msg);
+	public void testEmployee() {
+		ChatRoundabout cr = new ChatRoundabout(getConfig(), "cross-team");
+		cr.emailList();
 	}
+	
+//	@Test
+//	public void testEmailer() {
+//		LocalDate _nextFriday = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.FRIDAY));
+//		Time nextFriday = new Time(_nextFriday.toString());
+//		
+//		String logString = "Testing Emailer";
+//		
+//		// Send email
+//		if (initFlag) return;
+//		initFlag = true;
+//		File propsFile = new File(FileUtils.getWinterwellDir(), "open-code/goodloop.google/config/email.properties");
+//		if ( ! propsFile.exists()) {
+//			propsFile = new File(FileUtils.getWinterwellDir(), "logins/local.properties");
+//			if ( ! propsFile.exists()) {
+//				System.out.println("Please make a file with email login details here: "+propsFile);
+//				throw new ConfigException("Please symlink the logins/local.properties file or make a file with email login details here: "+propsFile+".");
+//			}
+//		}
+//		Properties props = FileUtils.loadProperties(propsFile);				
+//		EmailConfig ec = new EmailConfig();
+//		ec.emailServer = props.getProperty("emailServer").trim();
+//		ec.emailFrom = props.getProperty("emailFrom").trim();
+//		ec.emailPassword = props.getProperty("emailPassword").trim();
+//		ec.emailPort = 465;
+//		ec.emailSSL = true;		
+//		
+//		Emailer emailer = new Emailer(ec);
+//		String appName = "ChatRoundabout";
+//		String subject = appName+": Weekly Report "+nextFriday.toISOStringDateOnly();
+//		StringBuilder body = new StringBuilder();
+//		body.append("\r\nChatRoundabout ran on :"+nextFriday.toISOStringDateOnly());
+//		body.append("\r\n\r\n"+logString+"\r\n");
+//		
+//		InternetAddress from = emailer.getBotEmail();
+//		from.setAddress("wing@good-loop.com");
+//		InternetAddress email = emailer.getBotEmail();
+//		email.setAddress("wing@good-loop.com");
+//		SimpleMessage msg = new SimpleMessage(from, email, subject, body.toString());
+//		emailer.send(msg);
+//	}
 	
 	
 }
